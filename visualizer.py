43ba5bc031d5c9965247f3d9abb174e111a1a266
giorgio <giorgiosaldana99@gmail.com>
2024-07-08 17:19:00 +0200

    [edit] Edited to ensure the correctness of metrics for every algorithms

--- visualizer.py
+++ visualizer.py
@@ -1,11 +1,10 @@
-# visualizer.py
 import json
 import matplotlib.pyplot as plt
-import os
+import numpy as np
 import glob
+import pandas as pd
 
 def read_metrics(algorithm):
-    # This assumes your JSON files are named like 'metrics_<algorithm>_node_<node_id>.json'
     files = glob.glob(f'metrics_{algorithm}_node_*.json')
     data = {'convergence_time': [], 'message_count': [], 'node_id': []}
     for file in files:
@@ -16,35 +15,77 @@ def read_metrics(algorithm):
             data['node_id'].append(metrics['node_id'])
     return data
 
-def plot_data(data, algorithm):
-    fig, ax1 = plt.subplots()
+def plot_average_convergence_time(data):
+    algorithms = list(data.keys())
+    averages = [np.mean(data[alg]['convergence_time']) for alg in algorithms]
+    
+    fig, ax = plt.subplots()
+    ax.bar(algorithms, averages, color='lightblue')
+    ax.set_xlabel('Algorithm')
+    ax.set_ylabel('Average Convergence Time (s)')
+    ax.set_title('Average Convergence Time by Algorithm')
 
-    color = 'tab:red'
-    ax1.set_xlabel('Node ID')
-    ax1.set_ylabel('Convergence Time (s)', color=color)
-    ax1.bar(data['node_id'], data['convergence_time'], color=color)
-    ax1.tick_params(axis='y', labelcolor=color)
+    # Add a table below the chart
+    cell_text = [[f"{np.mean(data[alg]['convergence_time']):.4f}"] for alg in algorithms]
+    table = plt.table(cellText=cell_text, rowLabels=algorithms, colLabels=['Average Time (s)'],
+                      cellLoc='center', rowLoc='center', loc='bottom', bbox=[0.25, -0.6, 0.5, 0.5])
+    plt.subplots_adjust(left=0.2, bottom=0.4)
+    plt.show()
 
-    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-    color = 'tab:blue'
-    ax2.set_ylabel('Message Count', color=color)  # we already handled the x-label with ax1
-    ax2.plot(data['node_id'], data['message_count'], color=color, marker='o', linestyle='dashed')
-    ax2.tick_params(axis='y', labelcolor=color)
+def create_pandas_table(data):
+    df_list = []
+    for alg, metrics in data.items():
+        if alg in ['bully', 'ring']:
+            notation = r'$O(n^2)$' if alg == 'bully' else r'$O(n)$'
+            df = pd.DataFrame({
+                'Algorithm': [alg] * len(metrics['convergence_time']),
+                'Node ID': metrics['node_id'],
+                'Convergence Time': metrics['convergence_time'],
+                'Message Count': metrics['message_count'],
+                'O Notation': [notation] * len(metrics['convergence_time'])
+            })
+        else:
+            df = pd.DataFrame({
+                'Algorithm': [alg] * len(metrics['convergence_time']),
+                'Node ID': metrics['node_id'],
+                'Convergence Time': metrics['convergence_time']
+            })
+        df_list.append(df)
+    df_all = pd.concat(df_list, ignore_index=True)
+    return df_all
 
-    plt.title(f'Performance Metrics for {algorithm} Algorithm')
-    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+def save_table_as_image(df, filename):
+    fig, ax = plt.subplots(figsize=(12, 6))  # Adjust size as needed
+    ax.axis('tight')
+    ax.axis('off')
+    table = ax.table(cellText=df.values, colLabels=df.columns, cellLoc='center', loc='center')
+    plt.savefig(filename, bbox_inches='tight')
 
-    # Save the plot as an image file
-    plot_filename = f'plot_{algorithm}.png'
-    plt.savefig(plot_filename)
-    print(f"Plot saved as {plot_filename}")
+def plot_distribution(data):
+    fig, ax = plt.subplots()
+    for alg, metrics in data.items():
+        convergence_times = metrics['convergence_time']
+        ax.hist(convergence_times, bins=10, alpha=0.5, label=alg)
+    ax.set_xlabel('Convergence Time (s)')
+    ax.set_ylabel('Frequency')
+    ax.set_title('Distribution of Convergence Times')
+    ax.legend(loc='best')
+    plt.show()
 
 def main():
-    algorithms = ['bully', 'ring', 'proof_of_work', 'proof_of_stake']
+    algorithms = ['bully', 'ring', 'pow', 'pos']
+    all_data = {}
+
     for alg in algorithms:
         data = read_metrics(alg)
-        if data['node_id']:  # Only plot if there is data
-            plot_data(data, alg)
+        if data['node_id']:
+            all_data[alg] = data
+
+    if all_data:
+        plot_average_convergence_time(all_data)
+        df_all = create_pandas_table(all_data)
+        save_table_as_image(df_all, 'convergence_times_table.png')
+        plot_distribution(all_data)
 
 if __name__ == '__main__':
     main()
